44580f1d1c9eeafcc16c0fd254dd410a6672ebce
alesearm <alesearm@students.zhaw.ch>
2024-05-02 21:37:10 +0200

    PDFBox Einrichtung

--- app/src/main/java/test/App.java
+++ app/src/main/java/test/App.java
@@ -6,6 +6,17 @@ import com.indvd00m.ascii.render.api.IRender;
 import com.indvd00m.ascii.render.elements.PseudoText;
 import com.indvd00m.ascii.render.api.ICanvas;
 import com.indvd00m.ascii.render.api.IContextBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+
 import com.indvd00m.ascii.render.Render;
 
 public class App {
@@ -14,9 +25,10 @@ public class App {
     }
     
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(new App().getGreeting());
 
+    //ASCII Render
         IRender render = new Render();
         IContextBuilder builder = render.newBuilder();
         builder.width(120).height(20);
@@ -24,5 +36,25 @@ public class App {
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
         System.out.println(s);
+
+
+    //PDFBox
+        PDDocument helloPdf = new PDDocument();
+        PDPage page = new PDPage(PDRectangle.A4);
+        helloPdf.addPage(page);
+
+        PDPageContentStream contentStream = new PDPageContentStream(helloPdf, page);
+        contentStream.beginText();
+        contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 36);
+        contentStream.newLineAtOffset(10, 100);
+        contentStream.showText("Hello PDF World. My Name is Armin!!!");
+        contentStream.endText();
+        contentStream.close();
+
+        helloPdf.save(new File("C:\\Users\\armin\\DevOps\\PDFBox\\simple.pdf"));
+        helloPdf.close();
+
+
+
     }
 }
